6697c8993325d0648db42e69f0971539c69c9cbe
Theodorus Clarence <theodorusclarence@gmail.com>
2023-02-24 12:15:57 +0700

    feat: add toast query param system

--- src/components/DismissableToast.tsx
+++ src/components/DismissableToast.tsx
@@ -1,8 +1,37 @@
+import produce from 'immer';
+import { useRouter } from 'next/router';
 import * as React from 'react';
 import { toast, ToastBar, Toaster } from 'react-hot-toast';
 import { HiX } from 'react-icons/hi';
 
 export default function DismissableToast() {
+  const router = useRouter();
+  /**
+   * ? This allows to show a toast via query params
+   * * toast_type success, error, or none: default
+   * * toast_message (required) the message to show
+   */
+  React.useEffect(() => {
+    const toast_type = router.query.toast_type;
+    const toast_message = router.query.toast_message;
+
+    if (typeof toast_message === 'string') {
+      if (toast_type === 'success') {
+        toast.success(toast_message as string);
+      } else if (toast_type === 'error') {
+        toast.error(toast_message as string);
+      } else {
+        toast(toast_message as string);
+      }
+
+      const cleanedQuery = produce(router.query, (draft) => {
+        delete draft.toast_type;
+        delete draft.toast_message;
+      });
+      router.replace({ query: cleanedQuery }, undefined, { shallow: true });
+    }
+  }, [router, router.query]);
+
   return (
     <div>
       <Toaster
