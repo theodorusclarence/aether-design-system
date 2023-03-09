b8800d65fd4c0a23a08f3d191d8c089a504d8848
Theodorus Clarence <theodorusclarence@gmail.com>
2023-03-09 10:55:43 +0700

    feat: add SimpleCard component

---
+++ src/components/cards/SimpleCard.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+import clsxm from '@/lib/clsxm';
+
+const CARD_SIZE = ['sm', 'base'] as const;
+type CardSize = (typeof CARD_SIZE)[number];
+
+type SimpleCardProps = {
+  size?: CardSize;
+} & React.ComponentPropsWithoutRef<'div'>;
+
+export default function SimpleCard({
+  className,
+  size = 'base',
+  ...rest
+}: SimpleCardProps) {
+  return (
+    <div
+      className={clsxm(
+        'bg-white shadow-sm',
+        [
+          size === 'sm' && ['p-4', 'rounded-lg'],
+          size === 'base' && ['p-5', 'rounded-xl'],
+        ],
+        className
+      )}
+      {...rest}
+    />
+  );
+}
