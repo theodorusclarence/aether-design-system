9642fc1ec8f2869454ec9ed13189a2db2e3df0c0
rizqitsani <rizqitsani@gmail.com>
2024-01-03 21:28:32 +0700

    fix: missing test environment

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
 
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
 
   /**
    * Absolute imports and Module Path Aliases
